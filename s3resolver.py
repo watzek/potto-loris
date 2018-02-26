9502bdddad0b9919889da826706f00462d336523
budak <thatbudakguy@gmail.com>
2018-02-26 09:34:29 -0800

    rewrite s3resolver

--- s3resolver.py
+++ s3resolver.py
@@ -1,112 +1,124 @@
 # -*- coding: utf-8 -*-
-from loris.resolver import _AbstractResolver
-from loris.loris_exception import ResolverException
-from urllib import unquote
-import urlparse
+"""
+`s3resolver` -- Resolver for images coming from an AWS S3 bucket.
+==============================================================
+"""
+
 from os.path import join, exists
-import boto
+from urlparse import urlparse
+from urllib import unquote
 import logging
+import boto3
+import botocore
+
+from loris.resolver import _AbstractResolver
+from loris.loris_exception import ResolverException, ConfigError
+from loris.img_info import ImageInfo
 
 logger = logging.getLogger('webapp')
 
-
 class S3Resolver(_AbstractResolver):
     '''
-    Resolver for images coming from AWS S3 bucket.
+    Resolver for images stored in an Amazon Web Services S3 bucket. The first
+    call to `resolve()` copies the source image into a local cache; subsequent
+    calls use local copy from the cache.
+
     The config dictionary MUST contain
-     * `cache_root`, which is the absolute path to the directory where source images
-        should be cached.
-     * `source_root`, the s3 root for source images.
+     * `source_root`, the root directory for source images.
+
+    The config dictionary MAY contain
+     * `cache_root`, which is the absolute path to the directory where images
+        should be cached. Will default to /cache in the loris app directory.
     '''
     def __init__(self, config):
-        ''' setup object and validate '''
         super(S3Resolver, self).__init__(config)
-        self.cache_root = self.config.get('cache_root')
-        source_root = self.config.get('source_root')
-        assert source_root, 'please set SOURCE_ROOT in environment'
-        scheme, self.s3bucket, self.prefix, ___, ___ = urlparse.urlsplit(
-            source_root
-        )
-        assert scheme == 's3', '{0} not an s3 url'.format(source_root)
 
+        if 'cache_root' in self.config:
+            self.cache_root = self.config['cache_root']
+        else:
+            message = 'Configuration error: Missing setting for cache_root.'
+            logger.error(message)
+            raise ConfigError(message)
+
+        if 'source_root' in self.config:
+            self.source_root = self.config['source_root']
+        else:
+            message = 'Configuration error: Missing setting for source_root.'
+            logger.error(message)
+            raise ConfigError(message)
+
+        urlparts = urlparse(self.source_root)
+        self.s3_bucket = urlparts.netloc
+        self.prefix = urlparts.path
+
+        if not urlparts.scheme == 's3':
+            message = 'Configuration error: source_root is not an s3:// url.'
+            logger.error(message)
+            raise ConfigError(message)
+
+    def s3_key_name(self, ident):
+        '''Get the s3 key name of an image.'''
+        ident = unquote(ident)
+        return '{0}{1}'.format(self.prefix, ident)
+
+    def cache_file_path(self, ident):
+        '''Get the filename of a cached image.'''
+        ident = unquote(ident)
+        return join(self.cache_root, ident)
+
+    def in_cache(self, ident):
+        '''Check if a file is already in the cache.'''
+        return exists(self.cache_file_path(ident))
+
+    def copy_to_cache(self, ident):
+        '''Download the image to the cache.'''
+        s3_key_name = self.s3_key_name(ident)
+        cache_file_path = self.cache_file_path(ident)
+        try:
+            s3 = boto3.resource('s3')
+            s3.Bucket(self.s3_bucket).download_file(s3_key_name, cache_file_path)
+            logger.info('Downloaded key %s at: %s.', s3_key_name, cache_file_path)
+            return cache_file_path
+        except botocore.exceptions.ClientError as e:
+            logger.error(e)
+            raise
+
+    def raise_404_for_ident(self, ident):
+        '''Log the failure and throw a 404.'''
+        s3_key_name = self.s3_key_name(ident)
+        logger.warn(
+            'Key %s not found in bucket: %s.',
+            s3_key_name,
+            self.s3_bucket
+        )
+        raise ResolverException('Key %s not found.' % s3_key_name)
 
     def is_resolvable(self, ident):
-        '''does this file even exist?'''
-        ident = unquote(ident)
-        local_fp = join(self.cache_root, ident)
-        if exists(local_fp):
+        '''Check if the image is in the cache or available in the s3 bucket.'''
+        if self.in_cache(ident):
             return True
         else:
-            # check that we can get to this object on S3
-            s3 = boto.connect_s3()
-
+            s3_key_name = self.s3_key_name(ident)
             try:
-                bucket = s3.get_bucket(self.s3bucket, validate=False)
-            except boto.exception.S3ResponseError as e:
-                logger.error(e)
-                return False
-
-
-
-            if bucket.get_key(u'{0}{1}'.format(self.prefix, ident), ):
-                return True
+                s3 = boto3.resource('s3')
+                s3.Bucket(self.s3_bucket).Object(s3_key_name).load()
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == '404':
+                    self.raise_404_for_ident(ident)
+                else:
+                    logger.error(e)
+                    raise
             else:
-                logger.debug('AWS key %s does not exist' % (ident))
-                return False
+                return True
 
-
-    def resolve(self, ident):
-        '''get me the file'''
-        ident = unquote(ident)
-        local_fp = join(self.cache_root, ident)
-        logger.debug('local_fp: %s' % (local_fp))
- 
-        if exists(local_fp):
-            format = 'jp2' # FIXME
-            logger.debug('src image from local disk: %s' % (local_fp,))
-            return (local_fp, format)
-        else:
-            # get image from S3
-            bucketname = self.s3bucket 
-            keyname = '{0}{1}'.format(self.prefix, ident)
-            logger.debug('Getting img from AWS S3. bucketname, keyname: %s, %s' % (bucketname, keyname))    
-            
-            s3 = boto.connect_s3()
-            bucket = s3.get_bucket(bucketname, validate=False)
-            key = bucket.get_key(keyname, validate=False)
-            try:
-                res = key.get_contents_to_filename(local_fp)
-            except boto.exception.S3ResponseError as e:
-                message = 'Source image not found for identifier: %s.' % (ident,)
-                logger.warn(message)
-                raise ResolverException(404, message)
-            format = 'jp2' #FIXME
-            logger.debug('src format %s' % (format,))
-
-            return (local_fp, format)
-
-"""
-Copyright © 2017, Regents of the University of California
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-- Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-- Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-- Neither the name of the University of California nor the names of its
-  contributors may be used to endorse or promote products derived from this
-  software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
+    def resolve(self, app, ident, base_uri):
+        '''Return information about the image, caching it if needed.'''
+        if not self.is_resolvable(ident):
+            self.raise_404_for_ident(ident)
+        if not self.in_cache(ident):
+            self.copy_to_cache(ident)
+        cache_file_path = self.cache_file_path(ident)
+        format_ = self.format_from_ident(ident)
+        uri = self.fix_base_uri(base_uri)
+        extra = self.get_extra_info(ident, cache_file_path)
+        return ImageInfo(app, uri, cache_file_path, format_, extra)
